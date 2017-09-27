d7bdfe4ac3574b26b8c34f8517b6aa4cb537342f
Alexandre Da Silva <alexandre.dasilva@invenietis.com>
2017-09-27 14:34:46 +0200

    Little example

---
+++ src/ITI.PrimarySchool.WebApp/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITI.PrimarySchool.WebApp.Models.TestViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ITI.PrimarySchool.WebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public static readonly string[] AllowedColors = new [] { "R", "V", "B" };
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View( new BasicTestViewModel() );
+        }
+
+        [HttpPost]
+        public IActionResult Index( BasicTestViewModel model )
+        {
+            if( !AllowedColors.Contains( model.Color ) )
+            {
+                ModelState.AddModelError( "Color", "La couleur n'est pas autorisée" );
+            }
+
+            if( ModelState.IsValid )
+            {
+                return View( "Recap", model );
+            }
+
+            return View( model );
+        }
+    }
+}
---
+++ src/ITI.PrimarySchool.WebApp/Models/TestViewModels/BasicTestViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITI.PrimarySchool.WebApp.Models.TestViewModels
+{
+    public class BasicTestViewModel
+    {
+        [Required]
+        [Display( Name = "Couleur préférée" )]
+        public string Color { get; set; }
+
+        [Required]
+        [Range( 18, 25, ErrorMessage = "L'âge n'est pas compris entre les bornes" )]
+        [Display( Name = "Âge" )]
+        public int Age { get; set; }
+
+        [Required]
+        [Range( 94000, 95000, ErrorMessage = "Seuls les codes postaux du 94 sont autorisés" )]
+        [Display( Name = "Code postal" )]
+        public int PostalCode { get; set; }
+    }
+}
